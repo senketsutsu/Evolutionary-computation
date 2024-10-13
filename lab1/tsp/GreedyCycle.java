be89a1d99b8e973051e425d08eab91acdc5abfaf
senketsutsu <krasicka14@gmail.com>
2024-10-14 00:54:36 +0200

    final fixes GreedyCycle

--- lab1/tsp/GreedyCycle.java
+++ lab1/tsp/GreedyCycle.java
@@ -2,6 +2,8 @@ package lab1.tsp;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class GreedyCycle {
 
@@ -23,30 +25,34 @@ public class GreedyCycle {
         List<Integer> path = new ArrayList<>();
         path.add(startNode);
         visited[startNode] = true;
+        int nodesToSelect = (int) Math.ceil(n / 2.0);
 
         // all vertices have been added (50%)
-        while (path.size() * 2 < n) {
+        while (path.size() < nodesToSelect) {
             int bestNode = -1;
             double bestIncrementalCost = Double.MAX_VALUE;
             int bestPosition = -1;
 
             // insert into the current cycle in the best possible place the vertex
             // causing the smallest increase in cycle length
-            for (int candidateNode = 0; candidateNode < n; candidateNode++) {
-                if (!visited[candidateNode]) {
-                    for (int position = 0; position <= path.size(); position++) {
+
+            for (int position = 0; position <= path.size(); position++) {
+                for (int candidateNode = 0; candidateNode < n; candidateNode++) {
+                    if (!visited[candidateNode]) {
                         double incrementalCost = calculateIncrementalCost(path, distanceMatrix, candidateNode, position, nodes);
                         if (incrementalCost < bestIncrementalCost) {
                             bestIncrementalCost = incrementalCost;
                             bestNode = candidateNode;
                             bestPosition = position;
-                        }
+                            }
                     }
                 }
             }
 
-            path.add(bestPosition, bestNode);
-            visited[bestNode] = true;
+            if (bestNode >= 0 && bestPosition >= 0) {
+                path.add(bestPosition, bestNode); // Insert node at the best position
+                visited[bestNode] = true;
+            }
         }
 
         return path;
@@ -64,14 +70,14 @@ public class GreedyCycle {
      */
     private static double calculateIncrementalCost(List<Integer> path, double[][] distanceMatrix, int newNode, int position, double[][] nodes) {
         double cost = 0.0;
-        int previousNode = position > 0 ? path.get(position - 1) : newNode;
-        int nextNode = position < path.size() ? path.get(position) : newNode;
+        int previousNode = position > 0 ? path.get(position - 1) : path.get(path.size() - 1);
+        int nextNode = position < path.size() ? path.get(position) : path.get(0);
 
         cost += distanceMatrix[previousNode][newNode];
         cost += distanceMatrix[newNode][nextNode];
         cost += nodes[newNode][2];
 
-        if (position > 0) {
+        if (position > 0 || path.size() > 1) {
             cost -= distanceMatrix[previousNode][nextNode];
         }
 
